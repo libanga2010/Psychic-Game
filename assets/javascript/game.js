1fd53a4b332d3ae953d0e2cb9d054a327f245ae7
libanga2010 <libanga2010@gmail.com>
2017-02-19 09:29:12 -0500

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Letter choices available
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var computerChoices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 //Setting all to zero
 var wins = 0;
@@ -46,7 +46,7 @@ updateGuessesLeft();
 //When key is released it becomes the users guess
 document.onkeyup = function(event) {
     guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 
   guessedLetters.push(userGuess);
   updateGuessesLeft();
